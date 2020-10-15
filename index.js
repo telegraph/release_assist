ee5a08272971d3dfde788256c835fe93d626fbdb
jbropho <jorbrooks92@gmail.com>
2020-10-15 13:06:19 +0100

    syntax

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const github = require('@actions/github');
 
 async function run() {
   try {
-    const url: string = core.getInput('url');
+    const url = core.getInput('url');
     core.info('Sending POST request...');
     const data = JSON.parse(core.getInput('data'));
     await axios.post(url, data);
