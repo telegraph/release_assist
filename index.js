f290ef50190c48976a404efcd5843dea5c5bfd50
Jordan Brooks <33631766+jbropho@users.noreply.github.com>
2020-11-11 09:52:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const github = require('@actions/github');
 try {
     core.setOutput("label", github.context.payload.label.name)
     core.setOutput("pr_body", github.context.payload.pull_request.body);
+    core.setOutput("team_name", "platforms");
 } catch (error) {
     core.setFailed(error.message);
 }
