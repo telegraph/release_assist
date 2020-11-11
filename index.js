f3b19dba81a7401a8caba89db512d2ab9791131a
Jordan Brooks <33631766+jbropho@users.noreply.github.com>
2020-11-11 16:37:20 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const github = require('@actions/github');
 
 try {
     core.setOutput("label", github.context.payload.label.name)
-    //core.setOutput("pr_body", github.context.payload.pull_request.body);
-    core.setOutput("pr_body", "platforms");
-    core.setOutput("team_name", "platforms1");
+    core.setOutput("project_name", github.context.payload.repository.name);
+    // extract optional fields from PR body 
+    ks = ["team_name", "release_description", "release_version"];	    
+    ks.forEach(function(elem) {
+	    let re = new RegExp(`<!##(${elem})([\\\s\\\S]*?)##end`);
+	    res = github.context.payload.pull_request.body.match(re);
+        if (res != null) {
+	       core.setOutput(res[1], res[2].replace(/[\n\r]+/g, ' '))	
+	    }
+    });	
 } catch (error) {
     core.setFailed(error.message);
 }
