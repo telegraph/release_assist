3d5aa5164fc6fa181ac5749b8403bff5bb8f400d
Irfan Durrani <durranee@hotmail.com>
2020-12-02 11:39:55 +0000

    pr link

--- index.js
+++ index.js
@@ -8,17 +8,20 @@ async function run() {
   try {
     const data = {}
     const url = core.getInput('url');
-    
+    const pr_link = core.getInput('pr_link');
+
     // extract optional inputs and add them to POST request body
-    inputs = ['team_name', 'project_name', 'release_version', 'release_description']
+    inputs = ['team_name', 'project_name', 'release_version', 'release_description', 'pr_link']
     inputs.forEach(function(elem) {
        const val = core.getInput(elem);
        if (val != null && val != "") {
          data[elem] = val;
        }
     });
-    
+
+    console.log("printing things")
     console.log(data)
+    console.log(url)
     await axios.post(url, data);
   } catch (error) {
     core.setFailed(error.message);
