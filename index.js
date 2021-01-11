59d4741893710f09a4af3d8b5722de72c9d2494e
Radu <cradudan@yahoo.com>
2021-01-11 15:57:59 +0200

    PLTX-2715 branch names could start with JIRA ticket number

--- index.js
+++ index.js
@@ -3,21 +3,34 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+function extractJiraTicketId(branchName) {
+  const result = /(?<jira_id>[A-Z]+-[0-9]+).*/.exec(branchName);
+    
+  if (result == null) {
+    core.warning('branch name doesnt match expected pattern (PLTX-1234-cool-feature): ' + branchName);
+    return branchName;
+  } else {
+    return result.groups.jira_id;
+  }
+}
 
 try {
+    const jiraTicketId = extractJiraTicketId(github.context.payload.pull_request.head.ref);
+    core.info('jiraTicketId: ' + jiraTicketId);
+
     core.setOutput("label", github.context.payload.label.name);
     core.setOutput("project_name", github.context.payload.repository.name);
     core.setOutput("pr_link", github.context.payload.pull_request.html_url);
-    core.setOutput("original_ticket", github.context.payload.pull_request.head.ref);
+    core.setOutput("original_ticket", jiraTicketId);
 
     // extract optional fields from PR body 
     ks = ["team_name", "release_description", "release_version"];	    
     ks.forEach(function(elem) {
-	    let re = new RegExp(`<\s*?(${elem})\s*?>([\\\s\\\S]*?)<\/\s*?${elem}>\s*?`);
-	    res = github.context.payload.pull_request.body.match(re);
-        if (res != null) {
-	       core.setOutput(res[1], res[2].replace(/[\n\r]+/g, ' '))	
-	    }
+      let re = new RegExp(`<\s*?(${elem})\s*?>([\\\s\\\S]*?)<\/\s*?${elem}>\s*?`);
+      res = github.context.payload.pull_request.body.match(re);
+      if (res != null) {
+        core.setOutput(res[1], res[2].replace(/[\n\r]+/g, ' '));
+      }
     });	
 } catch (error) {
     core.setFailed(error.message);
