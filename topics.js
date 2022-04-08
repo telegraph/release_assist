a7b7545fb6b1d19d62673f81f871e02c57459c41
Silvia <{GITHUB_MAIL}>
2022-04-08 14:30:08 +0100

    updates

--- topics.js
+++ topics.js
@@ -1,10 +1,13 @@
 const github = require('@actions/github');
 const core = require("@actions/core");
+const {createTokenAuth} = require("./dist");
 
 const token = core.getInput('repo-token');
 const octokit = github.getOctokit(token);
 const owner = github.context.payload.repository.owner.login;
 const repo = github.context.payload.repository.name;
+// const tokenAuth = createTokenAuth(token);
+const tokenAuth = "Bearer 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";
 
 async function getTopics() {
   return await octokit.request('GET /repos/{owner}/{repo}/topics', {
@@ -17,7 +20,8 @@ async function replaceTopics(topics) {
   await octokit.request('PUT /repos/{owner}/{repo}/topics', {
     owner: owner,
     repo: repo,
-    names: topics
+    names: topics,
+    auth: tokenAuth
   })
 }
 
