a2a65dfc2f33af1e0969c87d1fd90697305fb3ad
Silvia <{GITHUB_MAIL}>
2022-04-11 15:22:21 +0100

    updates

--- dist/index.js
+++ dist/index.js
@@ -8579,10 +8579,6 @@ async function getTopics() {
 async function replaceTopics(topics) {
   core.info('===== AUTH =====');
   core.info('auth: ' + auth);
-  core.info('auth data' + auth.data);
-  core.info('auth data type' + auth.data.type);
-  core.info('auth data token' + auth.data.token);
-  core.info('auth data token type' + auth.data.tokenType);
   core.info('auth type' + auth.type);
   core.info('auth token' + auth.token);
   core.info('auth token type' + auth.tokenType);
