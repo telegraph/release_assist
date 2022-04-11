78ccbfcd2226b8237a88cf571c98c5b8d4c7efd9
Silvia <{GITHUB_MAIL}>
2022-04-11 13:59:16 +0100

    updates

--- topics.js
+++ topics.js
@@ -18,10 +18,16 @@ async function getTopics() {
 }
 
 async function replaceTopics(topics) {
-  core.info('request PUT /repos/{owner}/{repo}/topics');
+  core.info('===== AUTH =====');
+  core.info('auth: ' + auth);
+  core.info('auth data' + auth.data);
+  core.info('auth data type' + auth.data.type);
+  core.info('auth data token' + auth.data.token);
+  core.info('auth data token type' + auth.data.tokenType);
   core.info('auth type' + auth.type);
   core.info('auth token' + auth.token);
-  core.info('auth token type' + auth.typeType);
+  core.info('auth token type' + auth.tokenType);
+  core.info('request PUT /repos/{owner}/{repo}/topics');
   await request('PUT /repos/{owner}/{repo}/topics', {
     headers: authentication.headers,
     owner: owner,
