8087193246cdcb1686288fa9adbef2d17366b037
Silvia <{GITHUB_MAIL}>
2022-04-19 10:44:07 +0100

    updates

--- topics.js
+++ topics.js
@@ -13,7 +13,7 @@ async function getTopics() {
     owner,
     repo
   })
-  // This is working
+  // This works too
   // return await octokit.request('GET /repos/{owner}/{repo}/topics', {
   //   owner: owner,
   //   repo: repo
@@ -21,27 +21,40 @@ async function getTopics() {
 }
 
 async function replaceTopics(topics) {
-  const auth = createTokenAuth(token);
-  const authentication = await auth();
+  // const auth = createTokenAuth(token);
+  // const authentication = await auth();
+
   // core.info('===== AUTH =====');
   // core.info('auth: ' + auth);
   // core.info('auth type: ' + auth.type);
   // core.info('auth token: ' + auth.token);
   // core.info('auth token type: ' + auth.tokenType);
-  core.info('===== AUTHENTICATION =====');
-  core.info('auth: ' + authentication);
-  core.info('auth type: ' + authentication.type);
-  core.info('auth token: ' + authentication.token);
-  core.info('auth token type: ' + authentication.tokenType);
-  core.info('request PUT /repos/{owner}/{repo}/topics');
-  await request('PUT /repos/{owner}/{repo}/topics', {
-    headers: {
-      authorization: authentication.token
-    },
-    owner: owner,
-    repo: repo,
-    names: topics
+
+  // core.info('===== AUTHENTICATION =====');
+  // core.info('auth: ' + authentication);
+  // core.info('auth type: ' + authentication.type);
+  // core.info('auth token: ' + authentication.token);
+  // core.info('auth token type: ' + authentication.tokenType);
+  // core.info('request PUT /repos/{owner}/{repo}/topics');
+
+  core.info('You want this topics');
+  core.info(topics);
+
+  return await octokit.rest.repos.replaceAllTopics({
+    owner,
+    repo,
+    topics
   });
+
+  // await request('PUT /repos/{owner}/{repo}/topics', {
+  //   headers: {
+  //     authorization: authentication.token
+  //   },
+  //   owner: owner,
+  //   repo: repo,
+  //   names: topics
+  // });
+
   // core.info('octokit.request PUT /repos/{owner}/{repo}/topics');
   // await octokit.request('PUT /repos/{owner}/{repo}/topics', {
   //   owner: owner,
