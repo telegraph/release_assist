ad4262c99ba46878b280b046bb82171e309185cf
Silvia <{GITHUB_MAIL}>
2022-05-03 15:36:54 +0100

    updates

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ async function run() {
     else
       for (let index = 0; index < paths.length; index++) {
         core.info("Reading path: " + paths[index]);
-        let topics = (await readFile(paths[index])).replace(" ", "-").split(/\r?\n/);
+        let topics = (await readFile(paths[index])).replace(" ", /\r?\n/).split(/\r?\n/);
         core.info("Topics: " + topics);
         if(replace == "true")
           await replaceTopics(topics);
