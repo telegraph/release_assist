07ea872e3bae631d18abae899edfab0ee5aab414
Silvia <{GITHUB_MAIL}>
2022-05-03 14:34:35 +0100

    updates

--- dist/index.js
+++ dist/index.js
@@ -8755,7 +8755,7 @@ async function run() {
         core.info("Reading path: " + paths[index]);
         let newTopics = (await readFile(paths[index])).replace(" ", "-").split(/\r?\n/);
         core.info("New Topics: " + newTopics);
-        topics += newTopics;
+        topics = topics + "," + newTopics;
         // topics += (await readFile(paths[index])).replace(" ", "-").split(/\r?\n/);
       }
     core.info("Topics to add: " + topics);
