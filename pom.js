c74785e09f197529ba50a107056c3be0f59b1684
Silvia <{GITHUB_MAIL}>
2022-05-09 10:27:22 +0100

    updates

--- pom.js
+++ pom.js
@@ -8,7 +8,7 @@ function cleanPom(xml) {
         //Extract the value from the data element
         extractedData = result['project'];
         core.info(" ~ POM Extracted Data 1 ~ ");
-        core.info(result['project']['dependencies']['dependency'][0]['groupId'][0].toString());
+        core.info(result['project']['dependencies']['dependency'][0]['groupId'].toString());
     });
     core.info(" ~ POM Extracted Data 2 ~ ");
     core.info(extractedData.toString());
