09cb5ffbf340179ea4bc6911afd2475b5e2a5ab5
Silvia <{GITHUB_MAIL}>
2022-05-09 10:23:45 +0100

    updates

--- dist/index.js
+++ dist/index.js
@@ -15101,10 +15101,10 @@ function cleanPom(xml) {
         //Extract the value from the data element
         extractedData = result['project'];
         core.info(" ~ POM Extracted Data 1 ~ ");
-        core.info(result['project'].toString());
-        core.info(result['project']['dependencies'].toString());
-        core.info(result['project']['dependencies']['dependency'].toString());
-        core.info(result['project']['dependencies']['dependency']['groupId'][0].toString());
+        core.info(result['project']);
+        core.info(result['project']['dependencies']);
+        core.info(result['project']['dependencies']['dependency']);
+        core.info(result['project']['dependencies']['dependency'][0]['groupId'][0].toString());
     });
     core.info(" ~ POM Extracted Data 2 ~ ");
     core.info(extractedData.toString());
