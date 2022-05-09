11f34ad70c92a81942cdbd60d23d2262d5ed16dc
Silvia <{GITHUB_MAIL}>
2022-05-09 09:53:55 +0100

    updates

--- dist/index.js
+++ dist/index.js
@@ -15096,7 +15096,22 @@ const xml2js = __nccwpck_require__(1698);
 
 function cleanPom(pom) {
     core.info("POM: " + pom);
-    let parser = new xml2js.parseFromString(pom, "text/xml");
+    let parser = new xml2js.Parser();
+    parser.parseString(xml, function(err,result){
+        //Extract the value from the data element
+        extractedData = result['project']['repositories']['repository']['id'];
+        console.log(extractedData);
+    });
+
+
+
+    // parser.parseStringPromise(data).then(function (result) {
+    //     console.dir(result);
+    //     console.log('Done');
+    // })
+    //     .catch(function (err) {
+    //         // Failed
+    //     });
     return content;
 }
 
--- pom.js
+++ pom.js
@@ -3,7 +3,22 @@ const xml2js = require('xml2js');
 
 function cleanPom(pom) {
     core.info("POM: " + pom);
-    let parser = new xml2js.parseFromString(pom, "text/xml");
+    let parser = new xml2js.Parser();
+    parser.parseString(xml, function(err,result){
+        //Extract the value from the data element
+        extractedData = result['project']['repositories']['repository']['id'];
+        console.log(extractedData);
+    });
+
+
+
+    // parser.parseStringPromise(data).then(function (result) {
+    //     console.dir(result);
+    //     console.log('Done');
+    // })
+    //     .catch(function (err) {
+    //         // Failed
+    //     });
     return content;
 }
 
