3c44333fcfe8dc4dd78c1078b75371c5a1c84b12
Silvia <{GITHUB_MAIL}>
2022-05-09 10:36:01 +0100

    updates

--- dist/index.js
+++ dist/index.js
@@ -15099,13 +15099,12 @@ function cleanPom(xml) {
     let parser = new xml2js.Parser();
     parser.parseString(xml, function(err,result){
         //Extract the value from the data element
-        extractedData = result['project'];
-        core.info(" ~ POM Extracted Data 1 ~ ");
         core.info(JSON.stringify(result));
-        // core.info(result['project']['dependencies']['dependency'][0]['groupId'].toString());
+        core.info(result['project']['dependencies']['dependency'][0]['groupId'].toString());
+        extractedData = result['project']['dependencies'][0]['dependency'];
     });
     core.info(" ~ POM Extracted Data 2 ~ ");
-    core.info(extractedData.toString());
+    core.info(JSON.stringify(extractedData.toString()));
 
     // parser.parseStringPromise(data).then(function (result) {
     //     console.dir(result);
