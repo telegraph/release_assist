4e9b514ea6ac2a3bb8ed7d9598e36b8b4c660fa9
Silvia <{GITHUB_MAIL}>
2022-05-09 10:48:05 +0100

    updates

--- pom.js
+++ pom.js
@@ -6,7 +6,7 @@ function cleanPom(xml) {
     let parser = new xml2js.Parser();
     parser.parseString(xml, function(err,result){
         //Extract the value from the data element
-        extractedData = result['project']['dependencies'][0]['dependency']['groupId'];
+        extractedData = result['project']['dependencies'][0]['dependency'][0]['groupId'];
     });
     core.info(" ~ POM Extracted Data 2 ~ ");
     core.info(JSON.stringify(extractedData));
