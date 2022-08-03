e7d17effe1b22157b633ed819a5c7f0b4ea7f345
Silvia <{GITHUB_MAIL}>
2022-08-03 16:31:06 +0100

    paths not required

--- dist/index.js
+++ dist/index.js
@@ -15110,8 +15110,7 @@ function cleanPom(xml) {
                         core.info("Version is stored in Properties");
                         let variable_name = version.toString().slice(2, version.length-1).replace(/ /g,"");
                         core.info("Property Name: " + variable_name);
-                        core.info(result['properties'][variable_name]);
-                        version = result['properties'][variable_name];
+                        version = result['project']['properties'][0][variable_name];
                         core.info("Version: " + version);
                     }
                     artifacts.push(dependencies[index]["artifactId"] + "-" + version.toString().replace(/\./g, "-"));
