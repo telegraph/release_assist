c3dc80a69c8cc82ceb458dec9e777bb660c3ca9e
Silvia <{GITHUB_MAIL}>
2022-08-03 16:18:02 +0100

    paths not required

--- pom.js
+++ pom.js
@@ -13,7 +13,7 @@ function cleanPom(xml) {
                 let version = dependencies[index]["version"];
                 if (version) {
                     core.info("Version in POM: " + version);
-                    if (version.startsWith("$")) {
+                    if (version.toString().startsWith("$")) {
                         core.info("Version is stored in Properties");
                         let variable_name = str.slice(2, str.length-1).replace(/ /g,"");
                         core.info("Property Name: " + variable_name);
